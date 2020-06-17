4a93e5d6bb32b8e79367749beed539fdad53071c
Nafissa Iro <nafissairo@hotmail.com>
2020-06-17 16:17:31 -0400

    second jenkins project commit

--- src/test/java/model/CalculJenkinsTest.java
+++ src/test/java/model/CalculJenkinsTest.java
@@ -67,16 +67,32 @@ public class CalculJenkinsTest {
         assertEquals(expResult, result, 0.0);
         
     }
-       /**
+          /**
+     * Test of addition method, of class CalculJenkins.
+     */
+    @Test
+    public void testAddition2() {
+        System.out.println("addition");
+        double a = 22.0;
+        double b = 22.0;
+        CalculJenkins instance = new CalculJenkins();
+        double expResult = 44.0;
+        double result = instance.addition(a, b);
+        assertEquals(expResult, result, 0.0);
+       
+        
+    }
+
+    /**
      * Test of soustraction method, of class CalculJenkins.
      */
     @Test
-    public void testSoustractionEchec() {
+    public void testSoustraction2() {
         System.out.println("soustraction");
-        double a = 4.0;
-        double b = 2.0;
+        double a = 52.0;
+        double b = 30.0;
         CalculJenkins instance = new CalculJenkins();
-        double expResult = 3.0;
+        double expResult = 22.0;
         double result = instance.soustraction(a, b);
         assertEquals(expResult, result, 0.0);
         
